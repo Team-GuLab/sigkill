eb448034bb7f3b219f13ce57152d1eeb51226437
dgh06175 <dgh06175@gmail.com>
2026-01-29 14:13:45 +0900

    initialize Spring Boot project with Gradle setup and basic application structure

---
+++ sigkill-server/src/main/java/com/gulab/sigkillserver/SigkillServerApplication.java
@@ -0,0 +1,12 @@
+package com.gulab.sigkillserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigkillServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigkillServerApplication.class, args);
+    }
+}
---
+++ sigkill-server/src/test/java/com/gulab/sigkillserver/SigkillServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gulab.sigkillserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigkillServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
