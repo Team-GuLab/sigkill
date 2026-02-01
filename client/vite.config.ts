f4b7964e7fa55cc943899796b8f148bb3ce1bb27
wonza-hub <wonza8585@gmail.com>
2026-02-01 14:04:05 +0900

    feat: vitest 설치 및 초기 설정 #2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -13,6 +13,11 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/_tests/setup.ts",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
