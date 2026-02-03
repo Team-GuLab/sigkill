668a68513a760e6a80d70303f330ddec42f0c50b
wonza-hub <wonza8585@gmail.com>
2026-02-03 10:48:29 +0900

    rename: useRoomList 함수명 수정 #3

--- client/src/hooks/use-rooms.ts
+++ client/src/hooks/use-rooms.ts
@@ -2,11 +2,12 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { getRoomList, type RoomListParams } from "@/api/room";
 import { QUERY_KEYS } from "@/lib/constants";
 
-export function useRoomList(params: RoomListParams = {}) {
+export function useRooms(params: RoomListParams = {}) {
   return useSuspenseQuery({
     queryKey: QUERY_KEYS.room.list(params),
     queryFn: () => getRoomList(params),
-    staleTime: 0,
+    // staleTime: 10000,
+    gcTime: 0,
     refetchOnMount: true,
   });
 }
