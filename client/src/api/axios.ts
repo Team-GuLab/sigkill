b46b6bfc2a6cbf52d4a0a7081d180418d8f82e25
wonza-hub <wonza8585@gmail.com>
2026-02-03 17:24:00 +0900

    refactor: axios 인터셉터의 서버 에러 처리 로직 수정 #3

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -24,19 +24,24 @@ axiosInstance.interceptors.response.use(
   (error: AxiosError<APIErrorResponse>) => {
     // axios 에러가 아닌 경우 그대로 reject
     if (!axios.isAxiosError(error)) {
-      console.error("An unexpected error occurred", error);
+      console.error("❎ Not Axios error occurred", error);
       return Promise.reject(error);
     }
 
-    const { data, status } = error.response!;
+    if (error.response) {
+      console.error("🅾️ Axios error occurred", error);
+      const { status, data } = error.response;
+      // 애플리케이션 에러로 변환하여 reject
+      const appError = new AppError(
+        data?.message ?? "요청에 실패했습니다.",
+        data?.code ?? "UNKNOWN_ERROR",
+        status,
+      );
+      return Promise.reject(appError);
+    }
 
-    // 애플리케이션 에러로 변환하여 reject
-    const appError = new AppError(
-      data?.message ?? "요청에 실패했습니다.",
-      data?.code ?? "UNKNOWN_ERROR",
-      status,
-    );
-
-    return Promise.reject(appError);
+    // 알 수 없는 서버측 에러
+    console.error("🤯 An unexpected error occurred", error.toJSON());
+    return Promise.reject(error);
   },
 );
