5c53b5958100279107520f6894bf6f6d3a5e4737
dgh06175 <dgh06175@gmail.com>
2026-02-08 20:25:40 +0900

    refactor: #7 STOMP 핸들러에서 Authentication -> Principal로 타입 변경 및 불필요한 어노테이션 제거

--- sigkill-server/src/main/java/com/gulab/sigkillserver/config/security/StompHandler.java
+++ sigkill-server/src/main/java/com/gulab/sigkillserver/config/security/StompHandler.java
@@ -1,6 +1,5 @@
 package com.gulab.sigkillserver.config.security;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import org.springframework.core.Ordered;
@@ -12,12 +11,12 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.security.Principal;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class StompHandler implements ChannelInterceptor {
 
@@ -28,7 +27,7 @@ public class StompHandler implements ChannelInterceptor {
         // 메시지에서 STOMP 커맨드 추출
         if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
             // Spring Security가 핸드셰이크 시점에 주입한 Principal 가져오기
-            Authentication user = (Authentication) accessor.getUser();
+            Principal user = accessor.getUser();
 
             if (user == null) {
                 log.error("웹소켓 연결 실패: 인증 정보 없음 (세션 만료 또는 쿠키 누락)");
