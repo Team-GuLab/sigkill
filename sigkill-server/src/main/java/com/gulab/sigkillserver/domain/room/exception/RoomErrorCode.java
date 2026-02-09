e3dcea186319bcea5fcd592f0597c895ed0bc8ed
dgh06175 <dgh06175@gmail.com>
2026-02-09 11:31:11 +0900

    feat: #8 PLAYER_ALREADY_HOST 예외 코드 추가 및 적용

--- sigkill-server/src/main/java/com/gulab/sigkillserver/domain/room/exception/RoomErrorCode.java
+++ sigkill-server/src/main/java/com/gulab/sigkillserver/domain/room/exception/RoomErrorCode.java
@@ -22,8 +22,10 @@ public enum RoomErrorCode implements CustomErrorCodeInterface {
     ROOM_CREATE_ERROR("방 생성에 실패했습니다", HttpStatus.INTERNAL_SERVER_ERROR),
     ROOM_NUMBER_ERROR("방 번호는 4자리 정수여야 합니다", HttpStatus.BAD_REQUEST),
     ROOM_PAGING_PARAMETER_INVALID("페이지 번호와 페이지 크기는 0 이상의 정수여야 합니다", HttpStatus.BAD_REQUEST),
-    ALREADY_HOST("이미 호스트인 플레이어는 호스트로 변경할 수 없습니다", HttpStatus.BAD_REQUEST),
-    ROOM_ID_ALREADY_EXISTS("이미 존재하는 방 ID입니다", HttpStatus.CONFLICT);
+    ROOM_ID_ALREADY_EXISTS("이미 존재하는 방 ID입니다", HttpStatus.CONFLICT),
+    PLAYER_ALREADY_HOST("이미 호스트인 플레이어는 호스트로 변경할 수 없습니다", HttpStatus.BAD_REQUEST),
+    PLAYER_ID_ALREADY_EXISTS("이미 존재하는 플레이어 ID입니다", HttpStatus.CONFLICT),
+    PLAYER_NOT_FOUND("플레이어를 찾을 수 없습니다", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus httpStatus;
--- sigkill-server/src/main/java/com/gulab/sigkillserver/domain/room/model/Player.java
+++ sigkill-server/src/main/java/com/gulab/sigkillserver/domain/room/model/Player.java
@@ -1,6 +1,6 @@
 package com.gulab.sigkillserver.domain.room.model;
 
-import static com.gulab.sigkillserver.domain.room.exception.RoomErrorCode.ALREADY_HOST;
+import static com.gulab.sigkillserver.domain.room.exception.RoomErrorCode.PLAYER_ALREADY_HOST;
 
 import com.gulab.sigkillserver.common.exception.CustomException;
 import lombok.Getter;
@@ -39,7 +39,7 @@ public class Player {
 
     public void changeToHost() {
         if (this.role == PlayerRole.HOST) {
-            throw new CustomException(ALREADY_HOST);
+            throw new CustomException(PLAYER_ALREADY_HOST);
         }
         this.role = PlayerRole.HOST;
     }
