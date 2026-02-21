9305cc200f29a6447c75fee524e1c6b1923175bc
wonza-hub <wonza8585@gmail.com>
2026-02-21 20:08:36 +0900

    feat: 방 생성 명세 변경 건 반영 #41

--- client/src/hooks/room/use-create-room.ts
+++ client/src/hooks/room/use-create-room.ts
@@ -1,12 +1,12 @@
 import { createRoom } from "@/api/room";
-import type { CreateRoomParams, RoomItem } from "@/api/room/types";
+import type { CreateRoomParams, CreateRoomResponse } from "@/api/room/types";
 import { useMutation } from "@tanstack/react-query";
 import type { UseMutationCallback } from "@/shared/types";
 
 export function useCreateRoom(
-  callbacks?: UseMutationCallback<RoomItem, CreateRoomParams>,
+  callbacks?: UseMutationCallback<CreateRoomResponse, CreateRoomParams>,
 ) {
-  return useMutation<RoomItem, Error, CreateRoomParams>({
+  return useMutation<CreateRoomResponse, Error, CreateRoomParams>({
     mutationFn: (params: CreateRoomParams) => createRoom(params),
     onSuccess: callbacks?.onSuccess,
     onError: callbacks?.onError,
