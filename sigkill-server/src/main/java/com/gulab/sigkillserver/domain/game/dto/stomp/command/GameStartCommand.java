e1dd0961e6dd1b5527cbe13175b854937748cc5d
dgh06175 <dgh06175@gmail.com>
2026-02-23 14:51:48 +0900

    feat: #9 GameStartCommand 삭제

--- sigkill-server/src/main/java/com/gulab/sigkillserver/domain/game/dto/stomp/command/GameStartCommand.java
+++
@@ -1,8 +0,0 @@
-package com.gulab.sigkillserver.domain.game.dto.stomp.command;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record GameStartCommand(
-        @NotBlank String roomId
-) {
-}
