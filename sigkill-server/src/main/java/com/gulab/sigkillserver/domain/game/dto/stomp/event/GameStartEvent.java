70dc4c638f5224a19bb9c94d326254d6dc9ea342
dgh06175 <dgh06175@gmail.com>
2026-02-23 14:54:13 +0900

    feat: #9 GameStartEvent에서 발생 시점을 Instant로 처리하도록 수정

--- sigkill-server/src/main/java/com/gulab/sigkillserver/domain/game/dto/stomp/event/GameStartEvent.java
+++ sigkill-server/src/main/java/com/gulab/sigkillserver/domain/game/dto/stomp/event/GameStartEvent.java
@@ -1,6 +1,7 @@
 package com.gulab.sigkillserver.domain.game.dto.stomp.event;
 
 import com.gulab.sigkillserver.domain.game.dto.stomp.shared.GameStartPayload;
+import java.time.Instant;
 
 public record GameStartEvent(
         GameResponseType type,
@@ -9,12 +10,12 @@ public record GameStartEvent(
         long occurredAt,
         GameStartPayload payload
 ) {
-    public static GameStartEvent of(String roomId, Long gameId, long occurredAt, GameStartPayload payload) {
+    public static GameStartEvent of(String roomId, Long gameId, GameStartPayload payload) {
         return new GameStartEvent(
                 GameResponseType.GAME_START,
                 roomId,
                 gameId,
-                occurredAt,
+                Instant.now().toEpochMilli(),
                 payload
         );
     }
