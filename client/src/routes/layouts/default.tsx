3c08f5cb19645c9c1593b0a888035f98aab4ef42
wonza-hub <wonza8585@gmail.com>
2026-02-23 16:29:11 +0900

    design: 루트 레이아웃 헤더 제거 #54

--- client/src/routes/layouts/default.tsx
+++ client/src/routes/layouts/default.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router";
-import Header from "@/widgets/header";
 import { Toaster } from "@/ui/sonner";
 
 export default function DefaultLayout() {
   return (
     <>
-      <Header />
       <Toaster position="top-center" />
       <div className="bg-background min-h-screen px-4 py-8">
         <div className="mx-auto max-w-md">
