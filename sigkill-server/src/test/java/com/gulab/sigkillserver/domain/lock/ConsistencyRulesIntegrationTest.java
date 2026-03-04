6512351a132580901279e95fb659eb51e19b7d69
dgh06175 <dgh06175@gmail.com>
2026-03-04 15:52:10 +0900

    test: #86 스트레스 테스트를 위한 조건부 실행 및 태그 추가

--- sigkill-server/src/test/java/com/gulab/sigkillserver/domain/lock/ConsistencyRulesIntegrationTest.java
+++ sigkill-server/src/test/java/com/gulab/sigkillserver/domain/lock/ConsistencyRulesIntegrationTest.java
@@ -59,7 +59,9 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.InvocationInterceptor;
@@ -67,7 +69,8 @@ import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-//@Disabled
+@Tag("stress")
+@EnabledIfSystemProperty(named = "runStressTests", matches = "true")
 @ExtendWith(ConsistencyRulesIntegrationTest.RepeatAsSingleTestExtension.class)
 class ConsistencyRulesIntegrationTest {
     private static final int STRESS_ATTEMPTS = 100;
