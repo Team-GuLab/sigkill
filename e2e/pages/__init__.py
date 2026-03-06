1456ff2a2edd12fb2a31486d4977d44340484291
dgh06175 <dgh06175@gmail.com>
2026-03-06 12:35:04 +0900

    feat: 게임 결과 모달 화면을 위한 GameResultPage 클래스 추가

--- e2e/pages/__init__.py
+++ e2e/pages/__init__.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
 from pages.game_page import GamePage
+from pages.game_result_page import GameResultPage
 from pages.home_page import HomePage
 from pages.rooms_page import RoomsPage, CreateRoomDialog
 from pages.waiting_room_page import WaitingRoomPage
@@ -7,6 +8,7 @@ from pages.waiting_room_page import WaitingRoomPage
 __all__ = [
     "BasePage",
     "GamePage",
+    "GameResultPage",
     "HomePage",
     "RoomsPage",
     "CreateRoomDialog",
---
+++ e2e/pages/game_result_page.py
@@ -0,0 +1,42 @@
+from playwright.sync_api import Page, Locator, expect
+
+from pages.base_page import BasePage
+
+
+class GameResultPage(BasePage):
+    """게임 결과 모달 화면"""
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+
+    @property
+    def modal(self) -> Locator:
+        """결과 모달 루트(main.fixed.inset-0.z-50)"""
+        return self.page.locator("main.fixed.inset-0.z-50").first
+
+    @property
+    def heading(self) -> Locator:
+        """'경기 결과' 제목"""
+        return self.modal.get_by_role("heading", name="경기 결과", level=2)
+
+    @property
+    def confirm_button(self) -> Locator:
+        """결과 모달 확인 버튼"""
+        return self.modal.get_by_role("button", name="확인")
+
+    @property
+    def score_header(self) -> Locator:
+        """점수 컬럼 헤더"""
+        return self.modal.get_by_text("점수", exact=True)
+
+    def player_name(self, nickname: str) -> Locator:
+        """결과 모달 내 플레이어 닉네임"""
+        return self.modal.get_by_text(nickname, exact=True)
+
+    def wait_until_visible(self, timeout: int = 90_000) -> None:
+        """결과 모달 표시까지 대기"""
+        expect(self.heading).to_be_visible(timeout=timeout)
+
+    def click_confirm(self) -> None:
+        """확인 버튼 클릭"""
+        self.confirm_button.click()
