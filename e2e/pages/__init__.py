b2cf620c729a0e39bfacba242c1b952ce5490cb0
dgh06175 <dgh06175@gmail.com>
2026-03-06 12:05:51 +0900

    feat: 게임 진행/종료 화면을 위한 GamePage 클래스 추가

--- e2e/pages/__init__.py
+++ e2e/pages/__init__.py
@@ -1,10 +1,12 @@
 from pages.base_page import BasePage
+from pages.game_page import GamePage
 from pages.home_page import HomePage
 from pages.rooms_page import RoomsPage, CreateRoomDialog
 from pages.waiting_room_page import WaitingRoomPage
 
 __all__ = [
     "BasePage",
+    "GamePage",
     "HomePage",
     "RoomsPage",
     "CreateRoomDialog",
