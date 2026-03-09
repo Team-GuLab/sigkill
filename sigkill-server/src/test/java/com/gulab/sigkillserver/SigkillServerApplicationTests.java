071db50540cf7673ef337f08744e90be0442df5a
dgh06175 <dgh06175@gmail.com>
2026-03-09 14:43:31 +0900

    test: #102 테스트 환경에서 Redis 세션 저장소를 대체하도록 설정 추가

--- sigkill-server/src/test/java/com/gulab/sigkillserver/SigkillServerApplicationTests.java
+++ sigkill-server/src/test/java/com/gulab/sigkillserver/SigkillServerApplicationTests.java
@@ -1,12 +1,25 @@
 package com.gulab.sigkillserver;
 
+import java.util.concurrent.ConcurrentHashMap;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.MapSessionRepository;
 
 @SpringBootTest
 class SigkillServerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @TestConfiguration(proxyBeanMethods = false)
+    static class TestSessionConfig {
+
+        @Bean
+        MapSessionRepository sessionRepository() {
+            return new MapSessionRepository(new ConcurrentHashMap<>());
+        }
     }
 }
