697937fd1f71a93ac2cf656b084de46513ddf42c
wonza-hub <wonza8585@gmail.com>
2026-03-13 21:39:13 +0900

    fix: 대기방에서 토스트 메시지 표시안되는 문제 해결 #119
    - 인게임상태 전역상태에 추가
    - url 대신 인게임상태로 토스트표시 여부 판단

--- client/src/hooks/room/use-room-socket.ts
+++ client/src/hooks/room/use-room-socket.ts
@@ -12,7 +12,11 @@ import { handleRoomMessage } from "@/api/room/handle-room-message";
 import { subscribeError } from "@/api/error/subscribe-error";
 import { handleErrorMessage } from "@/api/error/handle-error-message";
 import { ROUTE_PATHS } from "@/routes/paths";
-import { useSetGameInfo, useSetGamePlayers } from "@/store/game-store";
+import {
+  useSetGameInfo,
+  useSetGamePlayers,
+  useSetIsInGame,
+} from "@/store/game-store";
 import { useResetRoom } from "@/store/room-store";
 
 /**
@@ -29,6 +33,7 @@ export const useRoomSocket = ({ roomId, myUserId }: UseRoomSocketProps) => {
   const navigate = useNavigate();
   const setGameInfo = useSetGameInfo();
   const setGamePlayers = useSetGamePlayers();
+  const setIsInGame = useSetIsInGame();
   const [isPending, setIsPending] = useState(false);
   const [isGameStarting, setIsGameStarting] = useState(false);
   const resetRoom = useResetRoom();
@@ -60,6 +65,7 @@ export const useRoomSocket = ({ roomId, myUserId }: UseRoomSocketProps) => {
         roomUnsubscribe.current = subscribeRoom(roomId, message => {
           if (message.type === "GAME_START") {
             isGameStarted.current = true;
+            setIsInGame(true);
             setGameInfo(message.roomId, message.gameId);
             setGamePlayers(message.payload.players);
             setIsGameStarting(true);
